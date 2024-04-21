7d5822c419189162d0fc59d201a04bda4b105cde
Bhishan Mahat <bhishanmahat.dev@gmail.com>
2024-04-21 22:54:13 +0545

    password hashing feature added using bcryptjs

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,12 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+
+import { db } from "@/drizzle/db";
+import { users } from "@/drizzle/schema";
 import { RegisterSchema } from "@/schemas/formSchema";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +15,18 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid Fields!" };
   }
 
-  return { success: "User created!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.insert(users).values({ name, email, password: hashedPassword });
+
+  // TODO: Send verification token email
+
+  return { success: "User registered!" };
 };
